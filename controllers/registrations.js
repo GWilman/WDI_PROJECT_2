5d02d687994a8dc31ba1617af74a9b94e6fe25ab
GWilman <georgewilman1@gmail.com>
2017-10-20 16:21:01 +0100

    password validation in place

--- controllers/registrations.js
+++ controllers/registrations.js
@@ -11,7 +11,12 @@ function createRoute(req, res){
       console.log(user);
       res.redirect('/');
     })
-    .catch((err) => res.status(500).end(err));
+    .catch((err) => {
+      if(err.name === 'ValidationError') {
+        return res.status(400).render('registrations/new', { message: 'Passwords do not match' });
+      }
+      res.status(500).end();
+    });
 }
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
+// require min password length?
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   imageURL: String
+});
+
+// YOU HAVE TO COMMENT OUT THE PASSWORD CONFIRMATION BEFORE RUNNING NODE DB/SEEDS
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPassword(next) {
+  if(this.isModified('password') && this._passwordConfirmation !== this.password) this.invalidate('passwordConfirmation', 'does not match');
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
