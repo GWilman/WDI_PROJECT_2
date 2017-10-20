bbd45d97bed17c6141d7629c1d82b4dc2fa36b24
GWilman <georgewilman1@gmail.com>
2017-10-20 15:00:09 +0100

    seeded user, working

--- db/seeds.js
+++ db/seeds.js
@@ -1,28 +1,31 @@
 const mongoose = require('mongoose');
-const { dbURI } = require('../config/environment');
+// const { dbURI } = require('../config/environment');
+const dbURI = 'mongodb://localhost/p-cont';
 
 mongoose.Promise = require('bluebird');
-mongoose.connect(dbURI);
+mongoose.connect(dbURI, { useMongoClient: true });
 
 const User = require('../models/user');
-const Post = require('../models/user');
+const Post = require('../models/post');
 
 User.collection.drop();
+Post.collection.drop();
 
 User
-  .create({
+  .create([{
     username: 'GWilman',
     email: 'george@george.com',
     password: 'password',
     passwordConfirmation: 'password'
-  })
+  }])
   .then((users) => {
+    console.log(`${users.length} users created`);
     return Post
-      .create({
+      .create([{
         caption: 'What a banger.',
         videoURL: 'https://www.youtube.com/watch?v=GXTV4pYVk20',
         createdBy: users[0]
-      });
+      }]);
   })
   .catch((err) => console.log(err))
   .finally(() => mongoose.connection.close());
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express    = require('express');
-const morgan     = require('morgan');
-const mongoose   = require('mongoose');
+const express        = require('express');
+const morgan         = require('morgan');
+const mongoose       = require('mongoose');
 // const bodyParser = require("body-parser");
-const port       = process.env.PORT || 3000;
-const router     = require('./config/routes');
-const dbURI      = process.env.MONGODB_URI || 'mongodb://localhost/p-cont';
+const port           = process.env.PORT || 3000;
+const router         = require('./config/routes');
+const dbURI          = process.env.MONGODB_URI || 'mongodb://localhost/p-cont';
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
 // Logging middleware
 app.use(morgan('dev'));
+
+app.use(expressLayouts);
 
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbURI, { useMongoClient: true });
